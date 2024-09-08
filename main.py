ebe77dcde82f69531306c5ab0fc6e5c76bfa804f
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-09-08 12:39:52 +0530

    Started working on init command.

--- main.py
+++ main.py
@@ -1,97 +1,63 @@
-contacts = {
-    "+10000100100": "John",
-    "+10001100001": "Alex"
-}
+import typer
+import os
+import json
+import uuid
 
-def show_app_menu():
-    """
-    show_app_menu This Function will display the app menu
-    """
-    print("1. View contacts")
-    print("2. Add contact")
-    print("3. Search contacts")
-    print("4. Exit")
-    
-    select_app_name()
-    
-def select_app_name():
-    repeat = True
-    while repeat == True:
-        try:
-            option = int(input("Choose[1-4]: "))
-        except ValueError:
-            print("Please input number")
-            # fill the option with value so the program won't error
-            option = 0
-        if option > 0 and option < 6: repeat = False
-        
-        if option == 1:
-            showContacts()
-            show_app_menu()
-        elif option == 2:
-            contact_str = str(input("Provide Contact Name and Number: "))
-            addContacts(contact_str=contact_str)
-            show_app_menu()
-        elif option == 3:
-            contact_number = str(input("Provide Contact Number: "))
-            searchContacts(number=contact_number)
-            show_app_menu()
-        elif option == 4:
-            print("Good bye!")
-            break
-        else:
-            print("Option unavailable.")
-            
-def showContacts():
-    """
-    showContacts Print out All Contacts in our Contact Book
-    """
-    
-    # manual numbering
-    num = 1
-    print("--------------------------------------") 
-    print(" No. | Name        | Number          |")
-    print("--------------------------------------")
-    for key, val in contacts.items():
-        # format to left-align and give some spacing
-        print("|{no} .| {name:<15} | {number:<15}|"
-        .format(no = num, name = val,
-                number = key))
-        num += 1
-    print("-------------------------------------")
-    
-def addContacts(contact_str: str):
-    """
-    addContacts Adds a contacts to your contact book 
+from rich import print
 
-    Args:
-        contact_str (str): Contact Name and Number Separated by coma (eg DemianPog,123456)
-    """
-    try:
-        name, number = contact_str.split(",")
+app = typer.Typer()
+SETTINGS_FILE = "database/settings.json"
+ACCOUNTS_FILE = "database/accounts.json"
 
-        # Check for duplicates
-        if number in contacts:
-            print("----- Contact with this number already exists! -----")
-        else:
-            contacts[number] = name
-            print("----- Contact Added Successfully -----")
 
-    except ValueError:
-        print("----- Invalid input format. Please use 'Name,Number' -----")
-    
-def searchContacts(number: str):
-    record = contacts.get(number, None)
-    if  not record:
-        print("------- Contact does not exist in our contact book -----")
-    else:
-        num = 1
-        print("-------------------------------------")
-        print("|No. | Name          | Number       |")
-        print("-------------------------------------")
-        print("|{no} .| {name:<15} | {number:<15}  |"
-            .format(no = num, name = record,
-                    number = number))
-        print("-------------------------------------")
+@app.command(name="init")
+def initialize():
+    while True:
+        username: str = input("Username: ")
 
-show_app_menu()
+        if len(username) <= 3:
+            print("[red]Your username should be of 4 letters.[/red]")
+            continue
+
+        password: str = input("Password: ")
+
+        break
+
+    create_files(username=username, password=password)
+
+    print("[green]App has been initialized.[/green]")
+
+
+def create_files(username: str, password: str):
+    settings_data = {
+        "initialized": True,
+        "current_account": None,
+        "accounts": [username]
+    }
+
+    with open(SETTINGS_FILE, "w") as file:
+        json.dump(settings_data, file, indent=4)
+
+    account_id = str(uuid.uuid4())
+
+    if not os.path.exists(ACCOUNTS_FILE) or os.path.getsize(ACCOUNTS_FILE) == 0:
+        with open(ACCOUNTS_FILE, "w") as file:
+            json.dump([], file, indent=4)
+
+    account_data_query = {
+        "id": account_id,
+        "username": username,
+        "password": password
+    }
+
+    with open(ACCOUNTS_FILE, "r") as file:
+        account_data = json.load(file)
+
+    account_data.append(account_data_query)
+
+    with open(ACCOUNTS_FILE, "w") as file:
+        json.dump(account_data, file, indent=4)
+
+
+if __name__ == "__main__":
+    app()
